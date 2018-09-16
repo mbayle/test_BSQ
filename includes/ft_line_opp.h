1ed97ec9a06269b9f0fd6fc00b34d249aa70672b
Gaspard BETTINGER <gabettin@student.42.fr>
2018-09-16 05:36:35 +0200

    save push before dodo

---
+++ includes/ft_line_opp.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_line_opp.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabettin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/16 05:16:05 by gabettin          #+#    #+#             */
+/*   Updated: 2018/09/16 05:32:16 by gabettin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LINE_OPP_H
+# define FT_LINE_OPP_H
+# include <stdlib.h>
+
+typedef struct	s_bigdata
+{
+	char	E;
+	char	W;
+	char	S;
+	int		Y;
+	int		X;
+}				t_bigdata;
+
+#endif
