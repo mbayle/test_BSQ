c72accbc57d42c99247ad791aa8a798971ed788d
Gaspard BETTINGER <gabettin@student.42.fr>
2018-09-18 04:18:35 +0200

    adjustment and norme on ft_find_square

---
+++ includes/ft_find_square.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_square.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabettin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/18 04:00:42 by gabettin          #+#    #+#             */
+/*   Updated: 2018/09/18 04:17:41 by gabettin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_FIND_SQUARE_H
+# define FT_FIND_SQUARE_H
+# include "ft_line_opp.h"
+
+typedef struct	s_fulldata
+{
+	int x_max_pos;
+	int y_max_pos;
+	int sizemax;
+	int x_depart;
+	int y_depart;
+	int x_actu;
+	int y_actu;
+	int size;
+}				t_fulldata;
+t_fulldata		*ft_find(char **tab, t_bigdata *data);
+t_fulldata		*ft_fulldata_init(void);
+#endif
---
+++ sources/ft_find_square.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tamere.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clboutry <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/18 02:51:29 by clboutry          #+#    #+#             */
+/*   Updated: 2018/09/18 04:17:35 by gabettin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_find_square.h"
+
+t_fulldata	*ft_find(char **tab, t_bigdata *data)
+{
+	t_fulldata	*r;
+
+	if ((r = ft_fulldata_init()) == 0)
+		return (0);
+	while (r->y_depart < (data->y - r->sizemax))
+	{
+		while (r->x_depart < (data->x - r->sizemax))
+		{
+			while (r->x_actu < (r->x_depart + r->size) &&
+					tab[r->y_actu][r->x_actu] != data->w
+					&& r->x_actu < data->x)
+				r->x_actu++;
+			while (r->y_actu < (r->y_depart + r->size)
+			&& tab[r->y_actu][r->x_actu] != data->w && r->y_actu < data->y)
+				r->y_actu++;
+			while (r->x_actu > r->x_depart &&
+				tab[r->y_actu][r->x_actu] != data->w && r->x_actu < data->x)
+				r->x_actu--;
+			if (r->size > r->sizemax)
+			{
+				r->x_max_pos = r->x_depart;
+				r->y_max_pos = r->y_depart;
+				r->sizemax = r->size;
+			}
+			if (tab[r->y_actu][r->x_actu] != 1 && r->y_actu < data->y &&
+					r->x_actu < data->x)
+			{
+				r->x_actu = r->x_depart + r->size;
+				r->y_actu = r->y_depart;
+				r->size++;
+			}
+			else
+			{
+				r->x_depart++;
+				r->x_actu = r->x_depart;
+				r->y_actu = r->y_depart;
+				r->size = 0;
+			}
+		}
+		r->x_depart = 0;
+		r->y_depart++;
+		r->size = 0;
+	}
+	return (r);
+}
+
+t_fulldata	*ft_fulldata_init(void)
+{
+	t_fulldata *result;
+
+	if ((result = malloc(sizeof(t_fulldata))) == 0)
+		return (0);
+	result->x_max_pos = 0;
+	result->y_max_pos = 0;
+	result->sizemax = 0;
+	result->x_depart = 0;
+	result->y_depart = 0;
+	result->x_actu = 0;
+	result->y_actu = 0;
+	result->size = 0;
+	return (result);
+}
